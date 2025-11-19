f6f46bf21a08ebe7de82df0f73612ade50f1c5d8
Adriano Canto Junior <adrianocanto462@gmail.com>
2025-11-19 15:13:20 -0300

    19/11 | 15:13

--- cadastro.php
+++ cadastro.php
@@ -1,3 +1,24 @@
+<?php
+
+include("functions/conexao.php");
+
+if (isset($_POST['submit'])) {
+
+    $nome = $_POST['nome'];
+    $usuario = $_POST['username'];
+    $senha = $_POST['password'];
+    $cpf = $_POST['cpf'];
+    $data_nascimento = $_POST['data'];
+    $email = $_POST['email'];
+
+    $result = mysqli_query($mysqli, "INSERT INTO usuario (nome, usuario, senha, cpf, data_nascimento, email) VALUES ('$nome', '$usuario', '$senha', '$cpf', '$data_nascimento', '$email')");
+
+    //header("Location: login.php");
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,43 +41,44 @@
         <img id="logo" src="img/loja_do_brasil_com_texto.png" alt="" style="width: 200px;">
         <div class="card" style="width: 300px;">
             <div class="card-body">
-                <div class="mb-3">
-                    <label for="">Nome Completo</label>
-                    <input id="nome" type="text" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="">Usuário</label>
-                    <input id="username" type="text" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="">Senha</label>
-                    <input id="password" type="password" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="">CPF</label>
-                    <input id="cpf" type="number" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="">Data de Nascimento</label>
-                    <input id="data" type="date" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="">Email</label>
-                    <input id="email" type="email" class="form-control">
-                </div>
-                <div class="d-grid gap-2">
-                    <button id="registrar" class="btn btn-primary" type="button">Registrar</button>
-                </div><br>
-                <div id="alert">
+                <form action="" method="post">
+                    <div class="mb-3">
+                        <label for="">Nome Completo</label>
+                        <input name="nome" id="nome" type="text" class="form-control" require>
+                    </div>
+                    <div class="mb-3">
+                        <label for="">Usuário</label>
+                        <input name="username" id="username" type="text" class="form-control" require>
+                    </div>
+                    <div class="mb-3">
+                        <label for="">Senha</label>
+                        <input name="password" id="password" type="password" class="form-control" require>
+                    </div>
+                    <div class="mb-3">
+                        <label for="">CPF</label>
+                        <input name="cpf" id="cpf" type="number" class="form-control" require>
+                    </div>
+                    <div class="mb-3">
+                        <label for="">Data de Nascimento</label>
+                        <input name="data" id="data" type="date" class="form-control" require>
+                    </div>
+                    <div class="mb-3">
+                        <label for="">Email</label>
+                        <input name="email" id="email" type="email" class="form-control" require>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <button id="registrar" class="btn btn-primary" type="submit">Registrar</button>
+                    </div><br>
+                    <div id="alert">
 
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </body>
 
 <script>
-
     var index = document.getElementById("logo")
 
     function home() {
@@ -64,11 +86,9 @@
     }
 
     index.addEventListener("click", home)
-
 </script>
 
 <script>
-
     const nome = document.getElementById("nome")
     const username = document.getElementById("username")
     const password = document.getElementById("password")
@@ -78,23 +98,23 @@
     const registrar = document.getElementById("registrar")
     const alerta = document.getElementById("alert")
 
-    function cadastro(){
-        if(nome.value === ""){
+    function cadastro() {
+        if (nome.value === "") {
             alerta.innerHTML = '<div class="alert alert-danger" role="alert">É necessário digitar seu nome para prosseguir!</div>'
         } else {
-            if(username.value === ""){
+            if (username.value === "") {
                 alerta.innerHTML = '<div class="alert alert-danger" role="alert">É necessário digitar seu usuário para prosseguir!</div>'
             } else {
-                if(password.value === ""){
+                if (password.value === "") {
                     alerta.innerHTML = '<div class="alert alert-danger" role="alert">É necessário digitar sua senha para prosseguir!</div>'
                 } else {
-                    if(cpf.value === ""){
+                    if (cpf.value === "") {
                         alerta.innerHTML = '<div class="alert alert-danger" role="alert">É necessário digitar seu CPF para prosseguir!</div>'
                     } else {
-                        if(data.value === ""){
+                        if (data.value === "") {
                             alerta.innerHTML = '<div class="alert alert-danger" role="alert">É necessário digitar sua data de nascimento para prosseguir!</div>'
                         } else {
-                            if(email.value === ""){
+                            if (email.value === "") {
                                 alerta.innerHTML = '<div class="alert alert-danger" role="alert">É necessário digitar seu mail para prosseguir!</div>'
                             } else {
                                 alerta.innerHTML = '<div class="alert alert-success" role="alert">Usuário Cadastrado com sucesso!</div>'
@@ -107,7 +127,6 @@
     }
 
     registrar.addEventListener("click", cadastro)
-
 </script>
 
 </html>
